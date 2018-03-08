e35a7dd5e6d33a448490095ec1ea2cfd205acbc7
MiguelRodriguezR <miguel.a.rodriguez.r@gmail.com>
2018-03-08 10:27:08 -0500

    created program to multiply matrices, lecture 2

--- Machine Learning - Siraj Raval/ Linear Algebra - MIT Gilbert Strang/Matrix.js
+++ Machine Learning - Siraj Raval/ Linear Algebra - MIT Gilbert Strang/Matrix.js
@@ -1,18 +1,21 @@
 class Matrix {
 
-  constructor(cols,rows) {
+  constructor(rows,cols) {
     this.matrix = new Array();
     this.cols = cols;
     this.rows = rows;
   }
-
+  fillPos(row,col,dat){
+    var aux = this.matrix[row]
+    aux[col]=dat;
+  }
   fill(args){
 
     for (var j = 1; j < this.rows+1; j++) {
-      console.log(this.rows);
+      //
       var row = new Array()
       for (var i = 0; i < this.cols; i++) {
-        //console.log(arguments[i]);
+        //
         if(((j*this.cols)-(this.cols-i))<arguments.length){
           row.push(arguments[((j*this.cols)-(this.cols-i))]);
         }
@@ -28,8 +31,13 @@ class Matrix {
   getMatrix(){
     return this.matrix
   }
+  getPos(row,col){
+      var aux = this.matrix[row]
+      return aux[col];
+  }
 }
 
-matrix = new Matrix(3,3);
-matrix.fill(1,2,3,4);
-matrix.getMatrix();
+// matrix = new Matrix(5,8);
+// matrix.fill();
+// matrix.getMatrix();
+// matrix.getPos(1,3);
