cd102f123719b22455c9f9b34fc34c41a6fd93f4
MiguelRodriguezR <miguel.a.rodriguez.r@gmail.com>
2018-04-02 13:14:55 -0500

    API CRUD

---
+++ Node - Nodeschool.io/Practice/6_apiCrudMysql/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXIST identation_war;
+
+CREATE DATABASE IF NOT EXIST identation_war;
+
+USE identation_war;
+
+CREATE TABLE team(
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  country VARCHAR(50) NOT NULL,
+  side VARCHAR(10) NOT NULL
+);
